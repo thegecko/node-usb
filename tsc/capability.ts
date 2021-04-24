3735cd855285ab819521fd7e542274210adae83c
thegecko <github@thegecko.org>
2021-04-24 22:41:03 +0100

    add capability class

---
+++ tsc/capability.ts
@@ -0,0 +1,18 @@
+import type { Device } from '*usb_bindings';
+
+export class Capability {
+    /** Object with fields from the capability descriptor -- see libusb documentation or USB spec. */
+    public descriptor: CapabilityDescriptor;
+
+    /** Integer capability type. */
+    public type: number;
+
+    /** Buffer capability data. */
+    public data: Buffer;
+
+    constructor(protected device: Device, protected id: number) {
+        this.descriptor = device._bosDescriptor!.capabilities[this.id];
+        this.type = this.descriptor.bDevCapabilityType;
+        this.data = this.descriptor.dev_capability_data;    
+    }
+}
