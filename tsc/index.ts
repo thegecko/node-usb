86960aaa1ea09c25daa8bc6455bd887efa064199
thegecko <github@thegecko.org>
2021-05-28 19:12:36 +0100

    WiP

--- tsc/index.ts
+++ tsc/index.ts
@@ -1,44 +1,6 @@
-import { promisify } from 'util';
-
 import * as usb from './usb';
 import { WebUSB } from './webusb';
 
-const findByIds = (vid: number, pid: number): usb.Device | undefined => {
-	const devices = usb.getDeviceList();
-	return devices.find(item => item.deviceDescriptor.idVendor === vid && item.deviceDescriptor.idProduct === pid);
-};
-
-const findBySerialNumber = async (serialNumber: string): Promise<usb.Device | undefined> => {
-	const devices = usb.getDeviceList();
-
-	for (const device of devices) {
-		try {
-			device.open();
-
-			const getStringDescriptor = promisify(device.getStringDescriptor).bind(device);
-			const buffer = await getStringDescriptor(device.deviceDescriptor.iSerialNumber);
-
-			if (buffer && buffer.toString() === serialNumber) {
-				return device;
-			}
-		} catch {
-			// Ignore any errors, device may be a system device or inaccessible
-		} finally {
-			try {
-				device.close();
-			} catch {
-				// Ignore any errors, device may be a system device or inaccessible
-			}
-		}
-	}
-
-	return undefined;
-};
-
 const toExport = usb;
-
-toExport.findByIds = findByIds;
-toExport.findBySerialNumber = findBySerialNumber;
 toExport.webusb = new WebUSB();
-
 export = usb;
--- tsc/usb.ts
+++ tsc/usb.ts
@@ -1,3 +1,4 @@
+import { promisify } from 'util';
 import { EventEmitter } from 'events';
 import { Interface } from './interface';
 import { Capability } from './capability';
@@ -277,5 +278,42 @@ usb.on('removeListener', event => {
 		usb._disableHotplugEvents();
 	}
 });
+
+const findByIds = (vid: number, pid: number): usb.Device | undefined => {
+	const devices = usb.getDeviceList();
+	return devices.find(item => item.deviceDescriptor.idVendor === vid && item.deviceDescriptor.idProduct === pid);
+};
+
+const findBySerialNumber = async (serialNumber: string): Promise<usb.Device | undefined> => {
+	const devices = usb.getDeviceList();
+
+	for (const device of devices) {
+		try {
+			device.open();
+
+			const getStringDescriptor = promisify(device.getStringDescriptor).bind(device);
+			const buffer = await getStringDescriptor(device.deviceDescriptor.iSerialNumber);
+
+			if (buffer && buffer.toString() === serialNumber) {
+				return device;
+			}
+		} catch {
+			// Ignore any errors, device may be a system device or inaccessible
+		} finally {
+			try {
+				device.close();
+			} catch {
+				// Ignore any errors, device may be a system device or inaccessible
+			}
+		}
+	}
+
+	return undefined;
+};
+
+const toExport = usb;
+
+toExport.findByIds = findByIds;
+toExport.findBySerialNumber = findBySerialNumber;
 
 export = usb;
